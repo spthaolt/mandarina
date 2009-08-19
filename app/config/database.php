46ea2e35a52fe3a508b517afc2ee714d97dd1792
Edwin Sandoval <sandovaledwin@hotmail.com>
2009-08-18 18:19:48 -0600

    These are the missing configuration needed for cakephp framework

--- app/config/database.php
+++ app/config/database.php
@@ -79,23 +79,23 @@
 class DATABASE_CONFIG {
 
 	var $default = array(
-		'driver' => 'mysql',
+		'driver'     => 'mysql',
 		'persistent' => false,
-		'host' => 'localhost',
-		'login' => 'user',
-		'password' => 'password',
-		'database' => 'database_name',
-		'prefix' => '',
+		'host'       => 'localhost',
+		'login'      => '',
+		'password'   => '',
+		'database'   => '',
+		'prefix'     => '',
 	);
 
 	var $test = array(
-		'driver' => 'mysql',
+		'driver'     => 'mysql',
 		'persistent' => false,
-		'host' => 'localhost',
-		'login' => 'user',
-		'password' => 'password',
-		'database' => 'test_database_name',
-		'prefix' => '',
+		'host'       => 'localhost',
+		'login'      => '',
+		'password'   => '',
+		'database'   => '',
+		'prefix'     => '',
 	);
 }
 ?>
